06e711acadc1cd6dceed24b87e088605e05564cb
Tapan Mayukh <email4mtapan@gmail.com>
2019-12-03 18:55:02 +0530

    Added Experiment 13

---
+++ Expt 13/expt13_1.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def LCG(n, t0, t1):
+	m = 714025
+	a = 1366
+	c = 150889
+	
+	x0 = 1
+	
+	random_nos = []
+	
+	for i in range(n):
+		xn = (a * x0 + c) % m
+		random_nos.append(xn)
+		
+		x0 = xn
+	
+	random_nos = np.array(random_nos)
+	random_nos = (t1 - t0) * random_nos / np.max(random_nos) + t0
+	
+	return random_nos
+	
+
+def fun(x):
+	return x * x
+
+
+def Monte_Carlo_int(n, t0, t1):
+	V = t1 - t0
+	
+	random_nos = LCG(n, t0, t1)
+	with open("data.dat", "w") as f:
+		for i in range(n):
+			f.write("{} {}\n".format(i + 1, random_nos[i]))
+	
+	intg = np.sum(fun(random_nos)) * V / n
+	
+	return intg
+
+
+if __name__ == "__main__":
+	n = 1000
+	
+	t0 = 0.0
+	t1 = 1.0
+	
+	intg = Monte_Carlo_int(n, t0, t1)
+	
+	print("Integral of the function with {} random points in ({}, {}): {}".format(n, t0, t1, intg))
---
+++ Expt 13/expt13_2.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def LCG(n, x0, t0, t1):
+	m = 714025
+	a = 1366
+	c = 150889
+	
+	random_nos = []
+	
+	for i in range(n):
+		xn = (a * x0 + c) % m
+		random_nos.append(xn)
+		
+		x0 = xn
+	
+	random_nos = np.array(random_nos)
+	random_nos = (t1 - t0) * random_nos / np.max(random_nos) + t0
+	
+	return random_nos
+	
+
+def fun(x, y):
+	return x * x + y * y
+
+
+def Monte_Carlo_int(n, t0, t1, y0, y1):
+	V = (t1 - t0) * (y1 - y0)
+	
+	random_nos_x = LCG(n, t0, t1)
+	random_nos_y = LCG(n, y0, y1)
+	
+	with open("data1.dat", "w") as f:
+		for i in range(n):
+			f.write("{} {}\n".format(random_nos_x[i], random_nos_y[i]))
+	
+	intg = np.sum(fun(random_nos_x, random_nos_y)) * V / n
+	
+	return intg
+
+
+if __name__ == "__main__":
+	n = 1000
+	
+	t0 = 0.0
+	t1 = 1.0
+	
+	y0 = 0.0
+	y1 = 1.0
+	
+	intg = Monte_Carlo_int(n, t0, t1, y0, y1)
+	
+	print("Integral of the function with {} random points in ({}, {}) X ({}, {}): {}".format(n, t0, t1, y0, y1, intg))
